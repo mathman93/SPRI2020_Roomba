ec5ddc10dc97454f4f49b7b6a1a1e6c0803c076a
Summit104 <ssarka5941@gmail.com>
2020-06-26 09:59:49 -0400

    Create Xbee_6_24.py

---
+++ Soumit code/Xbee_6_24.py
@@ -0,0 +1,44 @@
+import serial
+import time
+
+
+global Xbee #xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200)
+
+
+#Body
+send = time.time()
+on = 1
+angle = 0
+pulse = 0
+
+while True:
+	try:
+                while(angle < 360):
+                    def add():
+                    angle =+ 2.5 
+
+                    t = Timer(1, add)
+                    t.start()
+                    
+
+                if (angle = 360):
+                    angle = 0
+                    pulse = 1
+                
+
+                    
+		if (pulse = 1):
+			broadcast = '1'
+			Xbee.write(broadcast.encode())
+			pulse = 0
+			
+
+			
+		if Xbee.inWaiting() > 0: 
+			message = Xbee.read(Xbee.inWaiting()).decode()
+                        angle= angle - ((360 - angle)*0.25)
+
+                        
+                        
+		
