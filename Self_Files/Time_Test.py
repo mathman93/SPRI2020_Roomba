fe92b9ba62d225f7d307ee05fcc984f1ff63b6ed
jaxbyhimself <66624947+jaxbyhimself@users.noreply.github.com>
2020-07-01 09:31:48 -0400

    Update Time_Test.py

--- Self_Files/Time_Test.py
+++ Self_Files/Time_Test.py
@@ -39,7 +39,8 @@ while True:
                                 time_base=time_base+((360-((time.time()-time_base)*freq)/2)
                                 #angle+= something#based on value of phase
                                 #angle=angle+ x
-				
+
+
 	except KeyboardInterrupt:
 		print('')
 		break
