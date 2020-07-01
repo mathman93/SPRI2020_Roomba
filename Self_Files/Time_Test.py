2ecc7e6d16865f4dea71d0e0e8ffd1bc47b2a74a
jaxbyhimself <66624947+jaxbyhimself@users.noreply.github.com>
2020-07-01 09:26:46 -0400

    Update Time_Test.py

--- Self_Files/Time_Test.py
+++ Self_Files/Time_Test.py
@@ -25,29 +25,24 @@ while True:
         try:
                 phase= angle+((time.time()-time_base)*freq)
 
-                if phase >=threshold:
-                    #senf pulse amd resent to zero
-                    message = '1' # Change this to any character string you want
-                    Xbee.write(message.encode()) # Send the number over the Xbee
-                    print("you sent stuff")
-                    # Increase offset for next time to send message
-                    time_base+cycletime
-
-                if Xbee.inWaiting() > 0: # If there is something in the receive buffer
-                        message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
-                        print(message) # To see what the message is
+		if phase >= threshold:
+			message = '1' # Change this to any character string you want
+			Xbee.write(message.encode()) # Send the number over the Xbee
+			print("you sent stuff")
+			time_base += cycletime # Increase offset for next time to send message
+		
+		if Xbee.inWaiting() > 0: # If there is something in the receive buffer
+			message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
+			print(message) # To see what the message is
+			print(timer)
                         if message=='1':
-                            angle+= something#based on value of phase
-
-                            time_base=time_base+((360-((time.time()-time_base)*freq)/2)
-                            #angle=angle+ x
-
-
-
-
-
-        except KeyboardInterrupt:
-                print('')
+                                time_base=time_base+((360-((time.time()-time_base)*freq)/2)
+                                angle+= something#based on value of phase
+                                #angle=angle+ x
+				
+	except KeyboardInterrupt:
+		print('')
 		break
 
+        
 Xbee.close()
