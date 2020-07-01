5e27f0d7891626c89ed37ae36e6c12e46debf249
jaxbyhimself <66624947+jaxbyhimself@users.noreply.github.com>
2020-07-01 09:17:17 -0400

    Update Time_Test.py

--- Self_Files/Time_Test.py
+++ Self_Files/Time_Test.py
@@ -22,32 +22,32 @@ angle=int(input("intitial robot angle"))
 #phase = angle + timer
 
 while True:
-    try:
-        phase= angle+((time.time()-time_base)*freq)
+        try:
+                phase= angle+((time.time()-time_base)*freq)
 
-        if phase >=threshold:
-            #senf pulse amd resent to zero
-            message = '1' # Change this to any character string you want
-	    Xbee.write(message.encode()) # Send the number over the Xbee
-	    print("you sent stuff")
-	    # Increase offset for next time to send message
-            time_base+cycletime
+                if phase >=threshold:
+                    #senf pulse amd resent to zero
+                    message = '1' # Change this to any character string you want
+                    Xbee.write(message.encode()) # Send the number over the Xbee
+                    print("you sent stuff")
+                    # Increase offset for next time to send message
+                    time_base+cycletime
 
-        if Xbee.inWaiting() > 0: # If there is something in the receive buffer
-		message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
-		print(message) # To see what the message is
-		if message=='1':
-                    angle+= something#based on value of phase
+                if Xbee.inWaiting() > 0: # If there is something in the receive buffer
+                        message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
+                        print(message) # To see what the message is
+                        if message=='1':
+                            angle+= something#based on value of phase
 
-                    time_base=time_base+((360-((time.time()-time_base)*freq)/2)
-                    #angle=angle+ x
+                            time_base=time_base+((360-((time.time()-time_base)*freq)/2)
+                            #angle=angle+ x
 
 
 
 
 
         except KeyboardInterrupt:
-		print('')
+                print('')
 		break
 
 Xbee.close()
